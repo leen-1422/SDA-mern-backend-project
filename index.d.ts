283a8e72535660419aa505ec23324bfec385c8f1
Njood-917 <njood9177@gmail.com>
2023-11-20 13:51:56 +0300

    feat: commented unused files for now

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
-declare namespace Express {
-  interface Request {
-    msg: string
-    user: {
-      id: string
-      first_name: string
-    }
-  }
-}
+// declare namespace Express {
+//   interface Request {
+//     msg: string
+//     user: {
+//       id: string
+//       first_name: string
+//     }
+//   }
+// }
