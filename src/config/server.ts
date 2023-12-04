478d59538345c2caff289cd1cfa3de608e4ab5a3
leen-1422 <leen8288@yahoo.com>
2023-12-04 13:51:47 +0300

    fix

--- src/config/server.ts
+++ src/config/server.ts
@@ -4,7 +4,7 @@ export const dev ={
     app:{port: Number (process.env.PORT) ||3000},
     db:{
         url:
-        process.env.ATLAS_URL || 'mongodb+srv://leen77:leen77@cluster0.u62udj1.mongodb.net/SDA-Backend-Project'
+        process.env.ATLAS_URL 
     }
 
 }
