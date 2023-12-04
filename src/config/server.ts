672b8c8987506a9e0c4c9cc9f96438a44d3bdde9
leen-1422 <leen8288@yahoo.com>
2023-12-04 13:54:02 +0300

    fix

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,10 +1,8 @@
-import 'dotenv/config';
+import 'dotenv/config'
 
-export const dev ={
-    app:{port: Number (process.env.PORT) ||3000},
-    db:{
-        url:
-        process.env.ATLAS_URL 
-    }
-
+export const dev = {
+  app: { port: Number(process.env.PORT) || 3000 },
+  db: {
+    url: process.env.ATLAS_URL || '',
+  },
 }
